407aff29a2faf834d39b1e186e083d20de89adbe
Miles Hinchliffe <miles.hinchliffe@levellingup.gov.uk>
2025-06-29 12:32:08 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let form;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="form-container">
+    <form method="post">
+        <label for="exercise">Exercise:</label><br>
+        <input type="text" id="exercise" name="exercise"><br>
+        <button type="submit">Submit form</button>
+      </form>
+</div>
+
+{#if form?.value}
+  <p>You submitted: {form.value}</p>
+{/if}
+
