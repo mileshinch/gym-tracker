ad6479ec18c38df3dda0cd12293ebc0b5b145794
Miles Hinchliffe <miles.hinchliffe@levellingup.gov.uk>
2025-07-14 17:37:26 +0100

    Add state and props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,31 @@
 <script lang="ts">
-  export let form;
+  // none svelte rune way of doing things
+  // export let form;
+
+  let gymHistory = $state([]);
+
+  const { form } = $props();
+
+  let exercise = "";
+
+function addExercise() {
+  if (exercise.trim()) {
+    gymHistory.push(exercise.trim());
+    exercise = ""; // clear input
+    }
+  }
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <div class="form-container">
-    <form method="post">
+    <form method="post" on:submit|preventDefault={addExercise}>
         <label for="exercise">Exercise:</label><br>
-        <input type="text" id="exercise" name="exercise"><br>
+        <input type="text" id="exercise" name="exercise" placeholder="Exercise" required><br>
+        <!-- <label for="exercise">Weight:</label><br>
+        <input type="number" id="weight" name="weight" placeholder="Weight (kg)" required><br> -->
         <button type="submit">Submit form</button>
       </form>
 </div>
@@ -17,3 +34,10 @@
   <p>You submitted: {form.value}</p>
 {/if}
 
+
+<ul>
+{#each gymHistory as item}
+<li>{item}</li>
+{/each}
+  </ul>
+  
